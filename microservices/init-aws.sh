7317a454c5d6c8735264ffbf5d5bce325f66d633
Juliano Silva <cont.juliano@outlook.com>
2024-03-21 08:53:53 -0300

    build: added docker compose yml
    added init-aws.sh script

---
+++ microservices/init-aws.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+awslocal sqs create-queue --queue-name maked-order --region us-east-1
+awslocal sqs create-queue --queue-name confirmed-payment --region us-east-1
+
+awslocal dynamodb create-table \
+    --table-name order \
+    --key-schema AttributeName=ID,KeyType=HASH \
+    --attribute-definitions AttributeName=ID,AttributeType=S \
+    --billing-mode PAY_PER_REQUEST \
+    --region us-east-1
+
+awslocal dynamodb create-table \
+    --table-name payment \
+    --key-schema AttributeName=ID,KeyType=HASH \
+    --attribute-definitions AttributeName=ID,AttributeType=S \
+    --billing-mode PAY_PER_REQUEST \
+    --region us-east-1
