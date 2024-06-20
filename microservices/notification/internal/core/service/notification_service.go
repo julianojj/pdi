9c7b0d13be069ed87aa16ca562afb30bb0f335a1
Juliano <cont.juliano@outlook.com>
2024-06-20 01:15:01 -0300

    feat: add mongodb to notification ms

--- microservices/notification/internal/core/service/notification_service.go
+++ microservices/notification/internal/core/service/notification_service.go
@@ -1,25 +1,42 @@
 package service
 
-import "log/slog"
+import (
+	"log/slog"
+	"time"
+
+	"github.com/julianojj/pdi/notification/internal/ports"
+)
 
 type (
 	NotificationService struct {
-		logger *slog.Logger
+		logger                 *slog.Logger
+		notificationRepository ports.NotificationRepository
 	}
 )
 
 func NewNotificationService(
 	logger *slog.Logger,
+	notificationRepository ports.NotificationRepository,
 ) *NotificationService {
 	return &NotificationService{
 		logger,
+		notificationRepository,
 	}
 }
 
 func (ns *NotificationService) NotifyPaymentOrder(input map[string]any) error {
+	message := "Send message to"
 	ns.logger.Info(
-		"Send message to",
+		message,
 		"data", input,
 	)
+	notification := map[string]any{
+		"message":  message,
+		"customer": input["customer"],
+		"send_at":  time.Now().UTC(),
+	}
+	if err := ns.notificationRepository.Save(notification); err != nil {
+		return err
+	}
 	return nil
 }
